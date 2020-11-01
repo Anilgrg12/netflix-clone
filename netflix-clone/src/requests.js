651d8d7b6d6f80a27a75e95ab4ecdd65f4a96b85
Anil Gurung <anil_grg@hotmail.com>
2020-10-31 23:09:03 -0600

    addded reques s

--- netflix-clone/src/requests.js
+++ netflix-clone/src/requests.js
@@ -2,5 +2,7 @@ const API_KEY = "AIzaSyB7lFp8W_c2z5ksXsY139Jhsq-2v_hbhpk"
 
 const requests = {
     fecthTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOrginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`
+    fetchNetflixOrginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`, 
+    fetchTopRated:  `/movie/top_rated?api_key=${API_KEY}&language=en=-US`,
+    
 }

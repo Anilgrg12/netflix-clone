f238658253bafafd5b207006ccbed49d09c5a370
Anil Gurung <anil_grg@hotmail.com>
2020-11-01 19:10:56 -0700

    movies:

--- netflix-clone/src/requests.js
+++ netflix-clone/src/requests.js
@@ -4,5 +4,5 @@ const requests = {
     fecthTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOrginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`, 
     fetchTopRated:  `/movie/top_rated?api_key=${API_KEY}&language=en=-US`,
-    
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
 }

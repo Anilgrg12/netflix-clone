72b70eec0657f9b3dfa073019700890cb1c245cf
Anil Gurung <anil_grg@hotmail.com>
2020-11-08 17:59:13 -0700

    rendering movies lists

--- netflix-clone/src/components/Row.js
+++ netflix-clone/src/components/Row.js
@@ -1,16 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "../requests";
 
-function Row({ title, fetchUrl}) {
-    const [movies, setMovies] = useState([]);
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => { 
-        
-    },[])
-    return (
-        <div>
-           <h2> {title} </h2>
-        </div>
-    )
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2> {title} </h2>
+    </div>
+  );
 }
 
-export default Row
+export default Row;

ce479c62734af4ceda748223729ecd22fcc76bf6
Anil Gurung <anil_grg@hotmail.com>
2021-03-01 23:15:23 -0700

    refactoring

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -14,9 +14,9 @@ function Row({ title, fetchUrl }) {
   }, []);
 
   return (
-    <div className = "row">
+    <div className="row">
       <h2> {title} </h2>
-    <div className = "row_posters"></div>
+      <div className="row_posters"></div>
     </div>
   );
 }

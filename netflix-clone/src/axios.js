cf2856d5c9af952130d85f52437e791fd50868fb
Anil Gurung <anil_grg@hotmail.com>
2021-03-01 23:14:05 -0700

    adding new component

--- netflix-clone/src/axios.js
+++ netflix-clone/src/axios.js
@@ -1,5 +1,6 @@
 import Axios from "Axios";
-export default axios.create({
-  baseURL: "https://api.themoviedb.org/3"
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
 });
 
+export default instance;

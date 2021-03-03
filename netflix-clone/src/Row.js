259121010368f3cce0c1bba3d1ff68881c96ee03
Anil Gurung <anil_grg@hotmail.com>
2021-03-02 21:20:34 -0700

    row componenT

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -16,7 +16,7 @@ function Row({ title, fetchUrl }) {
   return (
     <div className="row">
       <h2> {title} </h2>
-      <div className="row_posters"></div>
+      <div className="row-posters"></div>
     </div>
   );
 }
